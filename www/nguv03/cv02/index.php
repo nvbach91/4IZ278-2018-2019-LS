14feaa29b739d45701a17aef3130b2f9537f5e89
Nguyen Viet Bach <nvbach91@outlook.com>
2019-02-12 23:00:14 +0100

    improved cv02

--- www/nguv03/cv02/index.php
+++ www/nguv03/cv02/index.php
@@ -1,73 +1,86 @@
 <?php
 
-class Person {
-    public function __construct($avatar, $firstName, $lastName, $title, $company, $available, $phone, $email, $website, $street, $propNumber, $orientationNumber, $city) {
-        $this->avatar = $avatar;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->title = $title;
-        $this->company = $company;
-        $this->available = $available;
-        $this->phone = $phone;
-        $this->email = $email;
-        $this->website = $website;
-        $this->street = $street;
-        $this->propNumber = $propNumber;
-        $this->orientationNumber = $orientationNumber;
-        $this->city = $city;
-    }
-    public function getAddress() {
-        return $this->street . ' ' . $this->propNumber . '/' . $this->orientationNumber . ', ' . $this->city;
-    }
+include './Person.php';
 
-    public function getAvailability() {
-        return $this->available ? 'Not available for contracts' : 'Now available for contracts';
-    }
-}
+$people = [];
 
-$person = new Person(
+array_push($people, new Person(
     'jedi-logo.svg',
     'Anakin',
     'Skywalker',
-    'Lead Developer / Architect',
+    'Temple Knight / Architect',
     'First Order Jedi Council',
     false,
-    '+420 776 889 643',
+    '+420 777 888 542',
     'skywalker@jedi-council.com',
     'www.jedi-council.com',
     'Temple of Eedit',
     42,
     121,
     'Coruscant',
-);
+));
 
-$person->abc = 'abcdef';
+array_push($people, new Person(
+    'jedi-master-logo.svg',
+    'Obi-wan',
+    'Kenobi',
+    'Master Artist / Lector',
+    'First Order Jedi Council',
+    true,
+    '+420 775 456 789',
+    'kenobi@jedi-council.com',
+    'www.jedi-council.com',
+    'Temple of Eedit',
+    43,
+    121,
+    'Coruscant',
+));
+
+array_push($people, new Person(
+    'galactic-republic-logo.svg',
+    'Padmé',
+    'Amidala',
+    'Senator of Naboo / Queen',
+    'the Galactic Senate',
+    true,
+    '+420 775 456 789',
+    'amidala@galactic-senate.com',
+    'www.galactic-senate.com',
+    'Senate District',
+    874,
+    12,
+    'Naboo',
+));
 
 ?>
 <?php include './header.php'; ?>
-<div class="business-card bc-front row">
-    <div class="col-sm-4">
-        <div class="logo" style="background-image: url(./img/<?php echo $person->avatar; ?>)"></div>
+<?php foreach($people as $person): ?>
+    <div class="row">
+        <div class="business-card bc-front row">
+            <div class="col-sm-4">
+                <div class="logo" style="background-image: url(./img/<?php echo $person->avatar; ?>)"></div>
+            </div>
+            <div class="col-sm-8">
+                <div class="bc-firstname"><?php echo $person->firstName; ?></div>
+                <div class="bc-lastname"><?php echo $person->lastName; ?></div>
+                <div class="bc-title"><?php echo $person->title; ?></div>
+                <div class="bc-company"><?php echo $person->company; ?></div>
+            </div>
+        </div>
+        <div class="business-card bc-back row">
+            <div class="col-sm-6">
+                <div class="bc-firstname"><?php echo $person->firstName; ?></div>
+                <div class="bc-lastname"><?php echo $person->lastName; ?></div>
+                <div class="bc-title"><?php echo $person->title; ?></div>
+            </div>
+            <div class="col-sm-6 contacts">
+                <div class="bc-address"><i class="fas fa-map-marker-alt"></i> <?php echo $person->getAddress(); ?></div>
+                <div class="bc-phone"><i class="fas fa-phone"></i> <?php echo $person->phone; ?></div>
+                <div class="bc-email"><i class="fas fa-at"></i> <?php echo $person->email; ?></div>
+                <div class="bc-website"><i class="fas fa-globe"></i> <?php echo $person->website; ?></div>
+                <div class="bc-available"><?php echo $person->getAvailability(); ?></div>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-8">
-        <div class="bc-firstname"><?php echo $person->firstName; ?></div>
-        <div class="bc-lastname"><?php echo $person->lastName; ?></div>
-        <div class="bc-title"><?php echo $person->title; ?></div>
-        <div class="bc-company"><?php echo $person->company; ?></div>
-    </div>
-</div>
-<div class="business-card bc-back row">
-    <div class="col-sm-6">
-        <div class="bc-firstname"><?php echo $person->firstName; ?></div>
-        <div class="bc-lastname"><?php echo $person->lastName; ?></div>
-        <div class="bc-title"><?php echo $person->title; ?></div>
-    </div>
-    <div class="col-sm-6 contacts">
-        <div class="bc-address"><i class="fas fa-map-marker-alt"></i> <?php echo $person->getAddress(); ?></div>
-        <div class="bc-phone"><i class="fas fa-phone"></i> <?php echo $person->phone; ?></div>
-        <div class="bc-email"><i class="fas fa-at"></i> <?php echo $person->email; ?></div>
-        <div class="bc-website"><i class="fas fa-globe"></i> <?php echo $person->website; ?></div>
-        <div class="bc-available"><?php echo $person->getAvailability(); ?></div>
-    </div>
-</div>
+<?php endforeach; ?>
 <?php include './footer.php'; ?>
