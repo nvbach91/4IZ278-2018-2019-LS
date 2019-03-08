0b34d012a0390e42febee0ceeac126b7f9305f5e
Nguyen Viet Bach <nvbach91@outlook.com>
2019-03-08 16:14:35 +0100

    fix

--- www/nguv03/cv05/oop/index.php
+++ www/nguv03/cv05/oop/index.php
@@ -17,9 +17,6 @@
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">About</a></li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="text" placeholder="Search">
