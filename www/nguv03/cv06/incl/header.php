db809f0ce580ad424d10ce5d6baa7ec084460fea
Nguyen Viet Bach <nvbach91@outlook.com>
2019-03-17 17:41:30 +0100

    refactoring

--- www/nguv03/cv06/incl/header.php
+++ www/nguv03/cv06/incl/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="mangomaniac, mango, mango, mango">
     <meta name="author" content="Nguyen Viet Bach">
-    <title>Mango Shop</title>
+    <title>Mango Shop | Mangomaniac Inc.</title>
     <link rel="shortcut icon" href="https://cdn.iconscout.com/icon/free/png-256/mango-fruit-vitamin-healthy-summer-food-31184.png">
     <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
--- www/nguv03/cv06/index.php
+++ www/nguv03/cv06/index.php
@@ -4,10 +4,10 @@
     <div class="row">
         <div class="col-lg-3">
             <h1 class="my-4">Mango shop</h1>
-            <?php require __DIR__ . '/components/Categories.php'; ?>
+            <?php require __DIR__ . '/components/CategoryDisplay.php'; ?>
         </div>
         <div class="col-lg-9">
-            <?php require __DIR__ . '/components/Slider.php'; ?>
+            <?php require __DIR__ . '/components/SlideDisplay.php'; ?>
             <?php require __DIR__ . '/components/ProductDisplay.php'; ?>
         </div>
     </div>
