e12000f605a319990166bd33b89680f3cf2e033b
Jakub Voráček <jakub.voracek@gmail.com>
2019-03-28 16:18:37 +0100

    edit it

--- www/vorj00/cv06/db/DB.php
+++ www/vorj00/cv06/db/DB.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/login.php';
+require __DIR__ . '/../../login.php';
 require_once __DIR__ . '/DatabaseInterface.php';
 
 abstract class Database implements DatabaseOperations
