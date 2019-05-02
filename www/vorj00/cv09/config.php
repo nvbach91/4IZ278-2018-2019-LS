91d3dc967c907072ed55df6b7d58acfa80745947
Jakub Voráček <jakub.voracek@gmail.com>
2019-05-02 14:53:04 +0200

    delete app id

--- www/vorj00/cv09/config.php
+++ www/vorj00/cv09/config.php
@@ -1,8 +1,8 @@
 <?php
 
 const CONFIG_FACEBOOK = [
-    'app_id' => '2294595784128584',
-    'app_secret' => 'b9b4e3bfa9dcfe43c808b8695a0e1a50',
+    'app_id' => '',
+    'app_secret' => '',
     'default_graph_version' => 'v2.10'
 ];   
 
